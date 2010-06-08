afa6d7783a5d26d9314cb56805fa35452f1ab444
aegypius <innercircle@aegypius.com>
2010-06-08 14:09:32 +0200

    Updating bootstrap script

--- public/index.php
+++ public/index.php
@@ -2,13 +2,16 @@
 /* --------------------------------------------------------------------
 	CONSTANTS
 -------------------------------------------------------------------- */
-define('APP_PATH',        realpath('..'));
-define('APP_CONFIG_PATH', realpath(APP_PATH . DIRECTORY_SEPARATOR . 'config'));
-define('APP_MODELS_PATH', realpath(APP_PATH . DIRECTORY_SEPARATOR . 'models'));
+define('APP_PATH',             realpath('..'));
+define('APP_CONFIG_PATH',      realpath(APP_PATH . DIRECTORY_SEPARATOR . 'config'));
+define('APP_MODELS_PATH',      realpath(APP_PATH . DIRECTORY_SEPARATOR . 'models'));
+define('APP_VIEWS_PATH',       realpath(APP_PATH . DIRECTORY_SEPARATOR . 'views'));
+define('APP_CONTROLLERS_PATH', realpath(APP_PATH . DIRECTORY_SEPARATOR . 'controllers'));
 
 set_include_path(
 	realpath('../../../libs') . PATH_SEPARATOR .
-	APP_MODELS_PATH . PATH_SEPARATOR .
+	APP_MODELS_PATH           . PATH_SEPARATOR .
+	APP_CONTROLLERS_PATH      . PATH_SEPARATOR .
 	get_include_path()
 );
 /* --------------------------------------------------------------------
