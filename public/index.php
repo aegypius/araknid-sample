2df0519ae72b24fade9b0fcdba052d239babb581
aegypius <innercircle@aegypius.com>
2010-08-03 12:36:11 +0200

    Fix include path expansion for libraries and controllers

--- public/index.php
+++ public/index.php
@@ -1,21 +1,24 @@
 <?php
 /* --------------------------------------------------------------------
-	CONSTANTS
+   CONSTANTS
 -------------------------------------------------------------------- */
 define('APP_PATH',             realpath('..'));
 define('APP_CONFIG_PATH',      realpath(APP_PATH . DIRECTORY_SEPARATOR . 'config'));
-define('APP_MODELS_PATH',      realpath(APP_PATH . DIRECTORY_SEPARATOR . 'models'));
-define('APP_VIEWS_PATH',       realpath(APP_PATH . DIRECTORY_SEPARATOR . 'views'));
+define('APP_LIBRARIES_PATH',   realpath(APP_PATH . DIRECTORY_SEPARATOR . 'libraries'));
 define('APP_CONTROLLERS_PATH', realpath(APP_PATH . DIRECTORY_SEPARATOR . 'controllers'));
+define('APP_MODELS_PATH',      realpath(APP_PATH . DIRECTORY_SEPARATOR . 'models'));
 
 set_include_path(
-	realpath('../../../libs') . PATH_SEPARATOR .
-	APP_MODELS_PATH           . PATH_SEPARATOR .
-	APP_CONTROLLERS_PATH      . PATH_SEPARATOR .
-	get_include_path()
+   implode(PATH_SEPARATOR, array(
+      realpath('../../../libs'),
+      APP_LIBRARIES_PATH,
+      APP_CONTROLLERS_PATH,
+      APP_MODELS_PATH,
+      get_include_path()
+   ))
 );
 /* --------------------------------------------------------------------
-	BOOTSTRAP
+   BOOTSTRAP
 -------------------------------------------------------------------- */
 require_once 'Araknid.php';
 
